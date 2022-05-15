fc5876c97a154e16c2189a1c00058de57485942c
StanDeMan <StanislausE@gmx.de>
2022-05-15 19:43:57 +0200

    Check interner connection

---
+++ GingerMintSoft.VersionParser/Internet/Connection.cs
@@ -0,0 +1,21 @@
+﻿namespace GingerMintSoft.VersionParser.Internet
+{
+    public static class Connection
+    {
+        public static async Task<bool> CheckAsync()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                using (await client.GetAsync("http://google.com/generate_204"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
